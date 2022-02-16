5c972ec77ee4a01329497fbaba5fef7f723c9c15
ARoman87 <angel427roman@gmail.com>
2022-02-16 15:56:39 -0500

    Portfolio New

---
+++ script.js
@@ -0,0 +1,12 @@
+let starts = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let middle = document.getElementById("middle");
+let front = document.getElementById("front");
+let text = document.getElementById("text");
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    moon.style.top = value * 1.05 + 'px';
+    middle.style.top = value * 0.1 + 'px';
+    text.style.left = value * 1.5 + 'px';
+})
